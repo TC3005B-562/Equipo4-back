4848cfab3f52c2e03ab7edbf08147590032b25ac
Diógenes Grajales Corona <a01653251@itesm.mx>
2022-04-01 20:29:27 -0600

    initial commit

---
+++ src/main/java/com/developmentsolutions/AmazonRSH/AmazonRshApplication.java
@@ -0,0 +1,13 @@
+package com.developmentsolutions.AmazonRSH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonRshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonRshApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/developmentsolutions/AmazonRSH/AmazonRshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developmentsolutions.AmazonRSH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonRshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
